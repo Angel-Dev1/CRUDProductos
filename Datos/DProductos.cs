a6125d42d87007753f68c6d02281cc998d7cc7ab
Xiuhyaotecatl <yaotlhuitl2000@outlook.com>
2023-02-27 12:28:58 -0600

    CRUD para productos

---
+++ Datos/DProductos.cs
@@ -0,0 +1,80 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+using Tiendaapi.Conexion;
+using Tiendaapi.Modelo;
+namespace Tiendaapi.Datos
+{
+    public class DProductos
+    {
+        Conexionbd cn = new Conexionbd();
+        public async Task<List<MProductos>> MostrarProductos()
+        {
+            var lista = new List<MProductos>();
+            using(var sql = new SqlConnection(cn.cadenaSQL()))
+            {
+                using(var cmd = new SqlCommand("mostrarProductos", sql)) 
+                {
+                    await sql.OpenAsync();
+                    cmd.CommandType = System.Data.CommandType.StoredProcedure;
+                    using (var item = await cmd.ExecuteReaderAsync())
+                    {
+                       while(await item.ReadAsync())
+                        {
+                            var mproductos =new MProductos();
+                            mproductos.id = (int)item["id"];
+                            mproductos.descripcion = (string)item["descripcion"];
+                            mproductos.precio = (decimal)item["precio"];
+                            lista.Add(mproductos);
+                        }
+                    }
+                }
+            }
+            return lista;
+        }
+
+        public async Task InsertarProductos(MProductos parametros)
+        {
+            using (var sql = new SqlConnection(cn.cadenaSQL()))
+            {
+                using (var cmd = new SqlCommand("InsertarProductos", sql))
+                {
+                    cmd.CommandType = System.Data.CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue
+                        ("@descripcion", parametros.descripcion);
+                    cmd.Parameters.AddWithValue("precio", parametros.precio);
+                    await sql.OpenAsync();
+                    await cmd.ExecuteNonQueryAsync();
+                }
+            }
+        }
+
+        public async Task EditarProductos(MProductos parametros)
+        {
+            using (var sql = new SqlConnection(cn.cadenaSQL()))
+            {
+                using (var cmd = new SqlCommand("EditarProductos", sql))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("id", parametros.id);
+                    cmd.Parameters.AddWithValue("precio", parametros.precio);
+                    await sql.OpenAsync();
+                    await cmd.ExecuteNonQueryAsync();
+                }
+            }
+        }
+
+        public async Task EliminarProductos(MProductos parametros)
+        {
+            using (var sql = new SqlConnection(cn.cadenaSQL()))
+            {
+                using (var cmd = new SqlCommand("EliminarProductos", sql))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("id", parametros.id);
+                    await sql.OpenAsync();
+                    await cmd.ExecuteNonQueryAsync();
+                }
+            }
+        }
+    }
+}
